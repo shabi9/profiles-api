5537a9a43abc6ceac96f098883015c41ef9c1775
Shahbaz <shabi9@gmail.com>
2020-05-13 01:01:48 +0300

    added permission token ability to login

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -7,6 +7,8 @@ from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 class HelloApiView(APIView):
     """api view class"""
@@ -111,3 +113,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
