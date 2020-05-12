1fe351dda1870bcf8dd9d87e506551c703eab3e6
Shahbaz <shabi9@gmail.com>
2020-05-13 00:47:26 +0300

    added searching user capabilities to api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,7 +6,7 @@ from rest_framework import viewsets
 from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
-
+from rest_framework import filters
 
 class HelloApiView(APIView):
     """api view class"""
@@ -109,3 +109,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.Objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
